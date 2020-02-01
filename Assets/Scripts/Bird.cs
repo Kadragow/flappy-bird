cf85da1d952286fe9e8268d1f782c69dcb1589a3
Kamil Blanik <kamil.lukasz.blanik@gmail.com>
2020-02-01 19:30:25 +0100

    Added playable version

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    public float jumpMultipler;
+    public float fallMultipler;
+    public int score;
+    public GameObject wing;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale != 0)
+        {
+            FallDown();
+            if (Input.GetKeyDown("space"))
+            {
+                Jump();
+            }
+            UpdateRotation(); 
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.collider.tag.Equals("ScoreGate"))
+        //{
+        //    Debug.LogError(collision.collider.name + "   " + collision.collider.tag);
+        //    score++;
+        //}
+        //else
+        //{
+        //}
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("ScoreGate"))
+        {
+            score++;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 50;
+        GUI.contentColor = Color.black;
+        GUI.Label(new Rect(0f, 0f, 200f, 200f), "Score: " + score);
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(0, 1, 0) * jumpMultipler;
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+        //TODO
+        //MoveWing();
+        //wing.transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    void FallDown()
+    {
+        rb.velocity -= new Vector3(0, 1, 0) * fallMultipler;
+    }
+
+    void UpdateRotation()
+    {
+        transform.Rotate(new Vector3(0, 0, rb.velocity.y / 10));
+    }
+
+    //TODO
+    void MoveWing()
+    {
+        wing.transform.eulerAngles = new Vector3(70, wing.transform.eulerAngles.y, wing.transform.eulerAngles.z);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public float DelayTime;
+
+    public GameObject obstacle;
+
+    private List<GameObject> obj;
+    private bool isGamePaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGamePaused = false;
+        InvokeRepeating("RenderObstacle", 0.0f, DelayTime);
+        obj = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isGamePaused)
+            {
+                Time.timeScale = 1f;
+                isGamePaused = false;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                isGamePaused = true;
+            }
+        }
+        List<GameObject> toRemove = new List<GameObject>();
+
+        foreach (var item in obj)
+        {
+            if(item.transform.position.x < -30f)
+            {
+                toRemove.Add(item);
+            }
+        }
+
+        foreach (var item in toRemove)
+        {
+
+            obj.Remove(item);
+            Destroy(item);
+        }
+    }
+
+    void RenderObstacle()
+    {
+        obj.Add(Instantiate(obstacle, new Vector3(30, Random.Range(-6, 6), 0), Quaternion.identity));
+    }
+
+    IEnumerator WaitForNext()
+    {
+        yield return new WaitForSeconds(DelayTime);
+    }
+}
